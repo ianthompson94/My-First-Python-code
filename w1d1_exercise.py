2fdcb3e8ba8a3b741b4b70577732cd1bed3cb8b0
ianthompson94 <136782167+ianthompson94@users.noreply.github.com>
2023-07-10 23:45:39 -0500

    Add files via upload
    This was my first Python script and exercise through VetsinTech

---
+++ w1d1_exercise.py
@@ -0,0 +1,17 @@
+# ITP Week 1 Day 1 Exercise
+
+# declare a VARIABLE named my_favorite_band and ASSIGN it a STRING value of your choice.
+
+myFavoriteBand="The Teskey Brothers"
+
+# PRINT a CONCATENATION of the variable and a string literal: " is my favorite band!"
+
+print(f"{myFavoriteBand} is my favorite band!")
+
+# prompt an user INPUT to ask "What is your favorite color?" and assign it to a variable called "user_favorite_color"
+
+user_favorite_color = input ("What is your favorite color?")
+
+# PRINT a string concatenation with user_favorite_color and "is a nice color!"
+
+print(f"{user_favorite_color} is a nice color!")
